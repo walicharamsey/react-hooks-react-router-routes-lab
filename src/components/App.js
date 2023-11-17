f0e760d61846913eb26b3e7a3217cf775dd8f25d
ramseypapii <ramsey.kuwona@student.moringaschool.com>
2023-11-18 00:36:40 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import Actors from "./Actors";
-import Directors from "./Directors";
-import Movies from "./Movies";
+// src/components/App.js
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import NavBar from './NavBar';
+import Home from './Home';
+import Movies from './Movies';
+import Directors from './Directors';
+import Actors from './Actors';
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/movies">
+          <Movies />
+        </Route>
+        <Route exact path="/directors">
+          <Directors />
+        </Route>
+        <Route exact path="/actors">
+          <Actors />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function NavBar() {
-  return <div>{/*{code here}*/}</div>;
-}
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <NavLink exact to="/">
+        Home
+      </NavLink>
+      <NavLink to="/movies">Movies</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+    </div>
+  );
+};
 
 export default NavBar;
